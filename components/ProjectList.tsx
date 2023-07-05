a872f34ddfbdf52cf84ecd3ed7319fce2a06df0f
moohabir <moohabir@gmail.com>
2023-07-05 22:40:22 +0200

    Dfg

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -4,13 +4,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-export default function ProjectList({ project }) {
+export default function ProjectList({ project }: any) {
   if (!project) {
     return null; // Return null or show a loading/error state when posts is undefined
   }
   return (
     <div className="">
-    
       <div className="rounded-xl bg-slate-500 py-4 mb-4 w-1/2 flex flex-col justify-center items-center m-auto md:w-1/2">
         {/*<Image
           src={urlForImage(project.mainImage.asset._ref).url()}
