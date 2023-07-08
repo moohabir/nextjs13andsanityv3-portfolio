ea40e8a3629b61c3b074449c3a31e80499b363b8
moohabir <moohabir@gmail.com>
2023-07-08 04:30:12 +0200

    dhh

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,93 +5,32 @@ import myPhoto from '../public/myphoto.jpg';
 import Image from 'next/image';
 import { Button } from '@mui/material';
 import Link from 'next/link';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Banner() {
-  const [showSecondHeading, setShowSecondHeading] = useState(false);
-
-  const firstHeadingVariants = {
-    hidden: {
-      opacity: 0,
-      x: '-50%',
-    },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        duration: 2,
-      },
-    },
-    exit: {
-      opacity: 0,
-      x: '-50%',
-      transition: {
-        duration: 2,
-      },
-    },
-  };
-
-  const secondHeadingVariants = {
-    hidden: {
-      opacity: 0,
-      x: '-50%',
-    },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        duration: 2,
-      },
-    },
-    exit: {
-      opacity: 0,
-      x: '-50%',
-      transition: {
-        duration: 2,
-      },
-    },
-  };
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSecondHeading((prev) => !prev);
-    }, 3000); // Adjust the delay to match the duration of each heading animation
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [showSecondHeading]);
-
   return (
-    <div className="text-center items-center flex justify-center gap-30 md:flex-col-reverse mb-10 pt-10 ">
+    <div className="text-center items-center flex justify-evenly gap-10 md:flex-col-reverse mb-10 pt-10 ">
       <div
         //className="flex-1"
-        style={{ minHeight: '150px' }}
+        style={{ width: '250px' }}
       >
-        <h2 className="font-bold text-2xl">Hello, I am</h2>
-        <AnimatePresence>
-          {!showSecondHeading ? (
-            <motion.h2
-              key="firstHeading"
-              className="font-bold text-3xl bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent "
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-              variants={firstHeadingVariants}
-            >
-              Mohamed Abdille,
-            </motion.h2>
-          ) : (
-            <motion.h2
-              className="font-bold text-3xl ml-4 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent"
-              initial="hidden"
-              animate="visible"
-              variants={secondHeadingVariants}
-            >
-              Frontend Web Developer
-            </motion.h2>
-          )}
-        </AnimatePresence>
-        <p className="text-black text-sm">I am javascript </p>
+        <h2 className="">Hello, I am </h2>
+        <span className="font-bold text-xl">Mohamed Abdille,</span>
+        <div className="flex ">
+          <h2 className='className="font-bold text-3xl text-black mr-2'>a</h2>
+          <TypeAnimation
+            sequence={['Frontend Web Developer', 2000, 'Freelancer', 2000]}
+            speed={50}
+            //wrapper="span"
+            repeat={Infinity}
+            //className="font-bold text-3xl bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent "
+            className="font-bold text-3xl text-black "
+          />
+        </div>
+
+        <p className="text-black text-sm">
+          I am javascript hgfghvhjbkjnknllnlkknlknsl
+        </p>
 
         <Link href="/contacts">
           <button className="text-white bg-black hover:bg-slate-600 w-48 rounded-full mt-5 py-4 ">
