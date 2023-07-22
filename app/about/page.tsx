46e830e183e294c29693ee6b1053c659f9a798e5
moohabir <moohabir@gmail.com>
2023-07-22 15:01:02 +0200

    modified:   app/about/page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,18 +19,22 @@ export default async function About() {
   const about = await client.fetch(query);
 
   return (
-    <div className=" flex flex-col justify-center items-center mt-10 mb-10 px-10 mx-auto p-4">
+    <div className=" text-center items-center flex justify-center gap-5 flex-col mt-10 mb-10 px-10 ">
       <h1 className="text-3xl font-bold mb-5 mt.5 ">About me</h1>
       {about.map((about: any) => (
-        <div key={about.id}>
+        <div
+          key={about.id}
+          className=" bg-slate-500 rounded-lg px-10 p-4 lg:mx-20 md:mx-0"
+        >
           <h1>{about.title}</h1>
-          <div className="flex justify-center gap-10 md:flex-col">
+          <div className="flex justify-center gap-10 md:flex-col p-4">
             <p className="text-sm">{about.description}</p>
             <Image
               src={urlForImage(about.image.asset._ref).url()}
               width={500}
               height={500}
               alt="hhh"
+              className="rounded-full"
             />
           </div>
         </div>
