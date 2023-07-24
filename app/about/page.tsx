696197ce3c4fb997834e16d46c216d48b79e34c6
moohabir <moohabir@gmail.com>
2023-07-24 14:16:31 +0200

    modified:   app/about/page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,23 +19,27 @@ export default async function About() {
   const abouts = await client.fetch(query);
 
   return (
-    <div className="text-center items-center flex justify-center gap-5 flex-col mt-10 pt-10 mb-10 mx-20 px-10">
+    <div className="text-center items-center flex justify-center gap-5 flex-col mt-10 pt-10 mb-10 ">
       <h1 className="font-bold text-3xl">About me</h1>
 
       <div className=" px-4 rounded-lg  py-4 ">
         {abouts.map((about: any) => (
           <div
             key={about._id}
-            className="rounded-xl bg-white gap-5  flex md:flex-col-reverse justify-center m-auto items-center px-20 mx-20 md:px-0 md:mx-0"
+            className="rounded-xl bg-white gap-5  flex md:flex-col-reverse justify-center m-auto items-center px-20 mx-10 md:px-0 md:mx-0"
           >
-            <Image
-              src={urlForImage(about.image.asset._ref).url()}
-              width={280}
-              height={280}
-              alt="hhh"
-              objectFit="cover"
-              className=" rounded-full px-10"
-            />
+            <div
+              className="rounded-full  p-4 "
+              style={{ position: 'relative', width: '250px', height: '250px' }}
+            >
+              <Image
+                src={urlForImage(about.image.asset._ref).url()}
+                alt="my profile picture here"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full "
+              />
+            </div>
 
             <div className="flex flex-col gap-5">
               <h2 className="font-bold ">{about.title}</h2>
