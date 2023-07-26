73e29671572c4ed14b6fa9a520094bf7bbbce3d1
moohabir <moohabir@gmail.com>
2023-07-27 01:48:52 +0200

    kjgd

--- app/(admin)/studio/[[...index]]/page.tsx
+++ app/(admin)/studio/[[...index]]/page.tsx
@@ -1,10 +1,15 @@
-'use client';
+import type { Metadata } from 'next';
+import { metadata as studioMetadata } from 'next-sanity/studio/metadata';
 
-import { NextStudio } from 'next-sanity/studio';
+import { Studio } from './Studio';
 
-import config from '../../../../sanity.config';
+// Set the right `viewport`, `robots` and `referer` meta tags
+export const metadata: Metadata = {
+  ...studioMetadata,
+  // Overrides the viewport to resize behavior
+  viewport: `${studioMetadata.viewport}, interactive-widget=resizes-content`,
+};
 
-export default function Studio() {
-  //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
-  return <NextStudio config={config} />;
+export default function StudioPage() {
+  return <Studio />;
 }
