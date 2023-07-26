72c2b703463571e1c7955f389eccff4274241e4f
moohabir <moohabir@gmail.com>
2023-07-27 01:38:58 +0200

    modified:   app/(admin)/studio/[[...index]]/page.tsx

--- app/(admin)/studio/[[...index]]/page.tsx
+++ app/(admin)/studio/[[...index]]/page.tsx
@@ -4,7 +4,7 @@ import { NextStudio } from 'next-sanity/studio';
 
 import config from '../../../../sanity.config';
 
-export function Studio() {
+export default function Studio() {
   //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
   return <NextStudio config={config} />;
 }
